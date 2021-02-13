8b5bf239732b9471c620a75d63b23d49c0d97ae6
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-13 13:30:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,11 +7,10 @@ end
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   the_manifest = {
-    "whale bone corsets" => 5
-    "porcelain vases" => 2
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2, 
     "oil paintings" => 3
   }
-
 end
 
 def retrieval
